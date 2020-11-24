c788aee78b32a451f18905446da063a4c43e8ba7
Jaime <jgaimer42@gmail.com>
2020-11-24 08:31:25 -0500

    cambios menores

--- API/main.go
+++ API/main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
-	"net/http"
-	"log"
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-
-type titular struct{
-	id string 
-	news_paper string 
-	title string 
-	link string 
-	date string 
+type titular struct {
+	id        string
+	newsPaper string
+	title     string
+	link      string
+	date      string
 }
-
 type allTitular []titular
 
 var titulares = allTitular{
-	{	
-		id:"grgwg",
-		news_paper:"wqdfwf",
-		title:"fewfew",
-		link:"efewfewf",
-		date:"ewefewf",
+	{
+		id:        "grgwg",
+		newsPaper: "wqdfwf",
+		title:     "fewfew",
+		link:      "efewfewf",
+		date:      "ewefewf",
 	},
 }
 
-func index(response http.ResponseWriter, request *http.Request){
-	fmt.Fprintf(response,"hola mundo")
+func index(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "hola mundo")
 }
 
-func getTitles(response http.ResponseWriter , request *http.Request){
+func getTitles(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(response).Encode(titulares)
 }
 
-func main(){
+func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/",index)
-	router.HandleFunc("/getTitles",getTitles)
-	log.Fatal(http.ListenAndServe(":3000",router))
+	router.HandleFunc("/", index)
+	router.HandleFunc("/getTitles", getTitles)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
